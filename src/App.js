248308d85f5990c8cd872324bf0e261678f52db5
Art Ruiz <a.ruiz.jr@gmail.com>
2022-04-24 15:24:30 -0500

    Simple Card render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import { Card } from "@mui/material";
 import React from "react";
 
-const App = () => <div>React MFE POC App</div>;
+const App = () => <Card sx={{ p: 3 }}>React MFE POC App</Card>;
 
 export default App;
